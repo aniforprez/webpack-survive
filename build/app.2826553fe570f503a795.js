e39a9c594c9e1b8511b5edc59968ffc184f946ab
Anirudh Sylendranath <ani.22691@gmail.com>
2016-06-08 12:12:19 +0530

    added webpackJsonp to mangle except

--- build/app.2826553fe570f503a795.js
+++ build/app.2826553fe570f503a795.js
@@ -1,2 +1,2 @@
-webpackJsonp([0,3],{0:function(n,e,o){var t=(o(1),o(34));document.body.appendChild(t())},34:function(n,e){n.n=function(){var n=document.createElement("h1");return n.e="pure-button",n.o="Hello again, friend of a friend",n}}});
+webpackJsonp([0,3],{0:function(n,e,o){var t=(o(1),o(34));document.body.appendChild(t())},34:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello again, friend of a friend",n}}});
 //# sourceMappingURL=app.2826553fe570f503a795.js.map
