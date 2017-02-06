1fccdaecc3682635ac8be81424c8b377bdf19731
Anirudh S <ani.22691@gmail.com>
2017-02-06 17:03:44 +0530

    Finished Linting Javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,84 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
-}
+    build: path.join(__dirname, 'build'),
+};
 
-module.exports = {
+const common = {
     entry: {
-        app: PATHS.app
+        app: PATHS.app,
     },
     output: {
         path: PATHS.build,
-        filename: '[name].js'
+        filename: '[name].js',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack Demo'
+            title: 'Webpack Demo',
+        }),
+    ],
+};
+
+const developmentConfig = {
+    devServer: {
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        },
+        historyApiFallback: true,
+        hotOnly: true,
+        stats: 'errors-only',
+        host: process.env.HOST,
+        port: process.env.PORT
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, 'node_modules')
+        ]),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                eslint: {
+                    failOnWarning: false,
+                    failOnError: true,
+
+                    fix: false,
+
+                    outputReport: {
+                        filePath: 'build/checkstyle.xml',
+                        formatter: require('eslint/lib/formatters/checkstyle')
+                    }
+                }
+            }
         })
-    ]
-}
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true
+                }
+            }
+        ]
+    }
+};
+
+module.exports = function(env) {
+    if(env == 'production') {
+        return common;
+    }
+
+    return Object.assign({}, common, developmentConfig, {
+        plugins: common.plugins.concat(developmentConfig.plugins),
+    });
+};
